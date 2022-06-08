7f5c87e52fb4b1a0c96c0ce4e3c360acfedc4b5a
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-06-08 14:47:48 -0700

    ...

--- src/components/AnimalCard.js
+++ src/components/AnimalCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function AnimalCard({ name, type, says, top, left }) {
+export default function AnimalCard({ name, type, says, }) {
   return (
     <div className='animal'>
       <p>{name}</p>
